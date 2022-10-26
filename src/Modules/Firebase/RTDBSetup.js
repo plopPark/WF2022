9fc10ee2df35d50e12c424f768ae5ef711b4c574
HoyeonLee <dlghdus122@gmail.com>
2022-10-27 07:50:07 +0900

    Changes

---
+++ src/Modules/Firebase/RTDBSetup.js
@@ -0,0 +1,22 @@
+/*
+https://firebase.google.com/docs/database/web/start
+ */
+import {initializeApp} from "firebase/app";
+import {getDatabase} from "firebase/database";
+
+// TODO: Replace the following with your app's Firebase project configuration
+// See: https://firebase.google.com/docs/web/learn-more#config-object
+const firebaseConfig = {
+    // ...
+    // The value of `databaseURL` depends on the location of the database
+    databaseURL: "https://wf2022-b0879-default-rtdb.firebaseio.com",
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+
+
+// Initialize Realtime Database and get a reference to the service
+const database = getDatabase(app);
+
+
